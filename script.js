e5c073b3d0728ffdf32b5929015ba41b87c164a2
Simon Zouki <srz08@mail.aub.edu>
2021-02-26 10:35:20 +0200

    phase 1 DONE

--- script.js
+++ script.js
@@ -1,41 +1,8 @@
 window.onload = function() { 
     var SERVER_URL = "http://127.0.0.1:5000"
     var addButton = document.getElementById("add-button");
-    addButton.addEventListener("click", addItem);
-    var sellUsdTransactions = [];
-    var buyUsdTransactions = [];
-    function addItem() {
-        var lbp = document.getElementById("lbp-amount").value
-        var usd = document.getElementById("usd-amount").value
-        var transType = document.getElementById("transaction-type").value
-        var diff = lbp / usd
-        if(transType === "usd-to-lbp"){
-            sellUsdTransactions.push(diff)
-        }
-        else {
-            buyUsdTransactions.push(diff)
-        }
-        updateRates();
-    }
+    addButton.addEventListener("click", addItem2);
 
-    function updateRates() {
-        var sum1 = 0;
-        if(sellUsdTransactions.length != 0 ) {
-        for (let value of sellUsdTransactions) {
-            sum1 += value;
-            }
-        var avg1 = sum1 / sellUsdTransactions.length
-        document.getElementById("sell-usd-rate").innerHTML = avg1 + " LBP per USD";
-        }
-        var sum2 = 0;
-        if(buyUsdTransactions.length != 0) {
-        for (let value of buyUsdTransactions) {
-            sum2 += value;
-            }
-        var avg2 = sum2 / buyUsdTransactions.length
-        document.getElementById("buy-usd-rate").innerHTML = avg2 + " USD per LBP";
-        }        
-    }
 
     function fetchRates() {
         fetch(`${SERVER_URL}/exchangeRate`)
@@ -44,10 +11,33 @@ window.onload = function() {
             var usd_to_lbp = data['usd_to_lbp']
             var lbp_to_usd = data['lbp_to_usd']
             document.getElementById("sell-usd-rate").innerHTML = usd_to_lbp + "USD per LBP"
-            document.getElementById("buy-usd-rate").innerHTML = usd_to_lbp + "LBP per USD"
+            document.getElementById("buy-usd-rate").innerHTML = lbp_to_usd + "LBP per USD"
        });
       }
-      fetchRates();
+    
+    function addItem2() {
+        var lbp = document.getElementById("lbp-amount").value
+        var usd = document.getElementById("usd-amount").value
+        var transType = document.getElementById("transaction-type").value
+        var trans=1;
+        if(transType === "usd-to-lbp") {
+            var trans = 0;
+        } 
+        fetch(`${SERVER_URL}/transaction`, {
+            method: 'post', 
+            headers: {
+                'Accept':'application/json',
+                'Content-Type': 'application/json'
+            },
+            credentials: 'same-origin',
+            redirect: 'manual',
+            body: JSON.stringify({
+                usd_amount: usd, 
+                lbp_amount:lbp, 
+                usd_to_lbp:trans}),
+        })
+        fetchRates();
+    }
 }
 
 
